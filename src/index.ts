6cc09cc693edb826bef5b4a6001a0238aa233523
luckstar77 <luckstar77y@gmail.com>
2022-10-06 17:34:17 +0800

    add mongodb client and update get stock api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-const app = express();
+import { connect as mongodbConnect } from './db/mongodb';
 
-app.get('/stock', function (req, res) {
-    const { search } = req.query;
-    res.send('Hello World');
-});
+const COLLECTION = 'stock';
 
-app.listen(3000);
+(async ()=> {
+    const mongodbClient = await mongodbConnect();
+    const app = express();
+    
+    app.get('/stock', async function (req, res) {
+        const { search } = req.query;
+        const stock = await mongodbClient.collection(COLLECTION).findOne({
+            $or:[{id: search},{name: search}]
+        });
+        res.send(stock);
+    });
+    
+    app.listen(3000);
+})();
