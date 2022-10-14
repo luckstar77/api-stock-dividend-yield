eaf7ef0f7c372e75cb5f32cd4a46d6deae897809
luckstar77 <luckstar77y@gmail.com>
2022-10-14 17:56:36 +0800

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { connect as mongodbConnect } from './db/mongodb';
+import cors from 'cors';
 
 const COLLECTION = 'stock';
 
 (async ()=> {
     const mongodbClient = await mongodbConnect();
     const app = express();
-    
+    app.use(cors({credentials:true}));
+
     app.get('/stock', async function (req, res) {
         const { search } = req.query;
         const stock = await mongodbClient.collection(COLLECTION).findOne({
